e6da85f88491c6c318cc3968414ce02132d650d9
ahmedwagih <ahmedxmen200@gmail.com>
2022-02-24 18:21:53 +0200

    Initial commit

---
+++ volcano/customer/models.py
@@ -0,0 +1,44 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,  **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    image = models.ImageField(default=None, blank=True, null=True, upload_to='uploads/')
+    phone_regex = RegexValidator(regex=r'^01[1|0|2|5][0-9]{8}$', message='phone must be an egyptian phone number...')
+    phone = models.CharField(null=False, validators=[phone_regex], max_length=14)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'address']
+
+    def get_full_name(self):
+        return self.first_name
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.email
---
+++ volcano/volcano/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
