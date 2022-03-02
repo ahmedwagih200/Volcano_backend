9f4bbc326df386c7199ad0d48caaf5dad01d96be
ahmedwagih200 <43096945+ahmedwagih200@users.noreply.github.com>
2022-03-02 10:46:01 +0200

    JWT Authentication / User API - ahmed wagih
    Co-authored-by: ahmedwagih <ahmedxmen200@gmail.com>

--- volcano/volcano/urls.py
+++ volcano/volcano/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
+
 ]
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
