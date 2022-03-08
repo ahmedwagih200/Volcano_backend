9b699308c4d331ff34861ea4718bfa517ca3b2a6
aya-98 <62079323+aya-98@users.noreply.github.com>
2022-03-08 23:46:02 +0200

    Menu (#3)
    * update menu
    
    * updating

--- volcano/volcano/urls.py
+++ volcano/volcano/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from products.views import *
+from adminpanel.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +14,18 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('category/<id>', get_product),
     path('categories', get_category),
-    path('cate/<id>', get_cate)
+    path('cate/<id>', get_cate) ,
+    path('order' , create_order) ,
+    path('orders/<id>' , get_orders) ,
+    path('order/<id>' , get_order),
+    path('adminpanel' , home),
+    path('admin-cate' , admin_cate ) ,
+    path('admin-prod' , admin_prod ),
+    path('admin-order' , admin_order ) ,
+    path('admin-user' , admin_user ) ,
+    path('del-cate<id>' , del_cate)
+
+    #path('test/<id>' , test)
 
 ]
 
